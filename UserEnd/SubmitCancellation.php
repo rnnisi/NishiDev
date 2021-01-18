159bc81c47a08f4fde1fcc043e7374bf7e7db5a8
Rebecca Nishide <rnnishide@gmail.com>
2021-01-17 19:10:58 -0800

    Add files via upload

---
+++ UserEnd/SubmitCancellation.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<h1>Cancel Experiment</h1>
+<h2>Format Guidelines</h2>
+<b>Job ID</b> should be in format "Req_"<br>
+<h2>Submit Request</h2>
+<form action = "GetCancellation.php" method = "get">
+Job ID: <input type="text" name="ID"><br>
+<input type="submit" value="Request Cancellation">
+</form>
+
+</body>
+</html>
---
+++ UserEnd/WipeRequests.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<h1>Queue has been emptied</h1>
+<?php
+$f = "requests.txt";
+if (!unlink($f)) {
+    echo "done<br><br>";
+} else {
+    echo "done<br><br>";
+}
+?>
+
+Back to <a id="home" href="/control_panel/control_panel.php">Control Panel</a>
+
+</body>
+</html>
